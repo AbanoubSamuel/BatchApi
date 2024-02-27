df494a9ae782e317b6412ca4ceede2c2d83a3950
Abanoub Samuel <abanoub.samuel@hotmail.com>
2024-02-27 13:47:08 +0200

    feat!: initial project files

---
+++ src/main/java/org/abg/batchapi/BatchApiApp.java
@@ -0,0 +1,13 @@
+package org.abg.batchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApiApp.class, args);
+    }
+
+}
---
+++ src/test/java/org/abg/batchapi/BatchApiAppTests.java
@@ -0,0 +1,13 @@
+package org.abg.batchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApiAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
