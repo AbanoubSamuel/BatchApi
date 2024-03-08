81ce41060233d2d956a01bdfd5f37cb1d06ea6c1
Abanoub Samuel <abanoub.samuel@hotmail.com>
2024-03-08 17:13:57 +0200

    edit!: removed unused code & imports

--- src/main/java/org/abg/batchapi/batch/VisitorItemProcessor.java
+++ src/main/java/org/abg/batchapi/batch/VisitorItemProcessor.java
@@ -1,24 +1,19 @@
 package org.abg.batchapi.batch;
 
 import org.abg.batchapi.entities.Visitor;
-import org.abg.batchapi.repositories.VisitorRepository;
 import org.abg.batchapi.utls.JsonResponse;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.SimpleDateFormat;
 
 @Component
 public class VisitorItemProcessor implements ItemProcessor<Visitor, Visitor> {
 
-    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy-HH:mm");
-
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private VisitorRepository visitorRepository;
+
 
     public VisitorItemProcessor(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
